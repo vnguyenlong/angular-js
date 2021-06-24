a8e48f910649e8671ddf4efb157c2ebe157ff918
l01659925574 <nvlong567@gmail.com>
2021-06-24 10:44:30 +0700

    update angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { DataService } from './data.service';
 
 import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
@@ -21,7 +22,7 @@ import { TodoListComponent } from './todo-list/todo-list.component';
     AppRoutingModule,
     FormsModule
   ],
-  providers: [],
+  providers: [DataService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,7 @@
-import { query, transition } from '@angular/animations';
+import { query, transition, style, trigger, animate, keyframes, stagger} from '@angular/animations';
 import { transformAll } from '@angular/compiler/src/render3/r3_ast';
 import { Component, OnInit } from '@angular/core';
+import { DataService } from '../data.service';
 
 @Component({
   selector: 'app-home',
@@ -20,19 +21,23 @@ export class  HomeComponent implements OnInit {
   btnText: string = 'Add an item';
   goalText: string ='My first life goal';
   goals: any[] = [];// cả đây nếu em ko để dữ liệu ra là any thì nó cũng lỗi
-  
-  constructor() { }
+
+
+  constructor(private _data: DataService) { }
 
   ngOnInit(): void {
+    this._data.goal.subscribe(res => this.goals = res);
     this.itemCount = this.goals.length;
-
+    this._data.changeGoal(this.goals);
   }
   addItem(){
     this.goals.push(this.goalText);
     this.goalText = '';
     this.itemCount = this.goals.length;
+    this._data.changeGoal(this.goals);
   }
-  removeItlem(i){// đoạn i này thì em chưa hiểu tại sao lỗi, nếu từ bên home.component
+  removeItlem(i :any){// đoạn i này thì em chưa hiểu tại sao lỗi, nếu từ bên home.component
     this.goals.splice(i, 1);
+    this._data.changeGoal(this.goals);
   }
 }
